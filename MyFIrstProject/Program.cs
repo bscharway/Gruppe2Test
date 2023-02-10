4f127859414d1777b53d29f4fba479281b0eaecc
Bright Charway <brightsswagness@Brights-MBP.webspeed.dk>
2023-02-10 20:03:24 +0100

    first commit

---
+++ MyFIrstProject/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace MyFIrstProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, ucl!");
+
+            Console.Write("hvad er dit fornavn? ");
+
+            string firstName;
+            firstName = Console.ReadLine();
+
+            Console.WriteLine("Velkommen " + firstName);
+
+            Console.Write("Hvad er dit Efternavn? ");
+
+            string lastName;
+            lastName = Console.ReadLine();
+            Console.WriteLine("HEj med dig " + firstName +" "+ lastName );
+
+            Console.ReadLine();
+        }
+    }
+}
